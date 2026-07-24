718f341c872cccc86291bdc95d19b155bd362f3e
Saad Larhrib <saad.larhrib06@gmail.com>
2026-07-25 00:17:10 +0100

    Now its show the domain name like 'google.com' and parse the question part, and now working on the anwser part

--- include/dns.h
+++ include/dns.h
@@ -4,6 +4,7 @@
 #include <stddef.h>
 
 
-void parse_dns(unsigned char* dns_pylod, size_t dsn_size);
+void parse_dns(const unsigned char* dns_pylod, size_t dsn_size);
+const unsigned char *parse_question(const unsigned char *question);
 
 #endif
--- src/dns.c
+++ src/dns.c
@@ -24,29 +24,66 @@ void parse_dns(const unsigned char* dns_payload, size_t dns_size){
     const dnshdr* dns = (const dnshdr*)dns_payload;
     
     printf("$$$$$$$$$$$$$$$$$$$$$$$$ DNS $$$$$$$$$$$$$$$$$$$$$$$\n");
-    printf("Transaction ID          : 0x%04X\n", ntohs(dns->id));
-    printf("Flag                    : %u\n", ntohs(dns->flag));
-    printf("Questions               : %u\n", ntohs(dns->qdcount));
-    printf("Answers                 : %u\n", ntohs(dns->ancount));
-    printf("Num Authority RRS       : %u\n", ntohs(dns->num_authority_rrs));
-    printf("Num Additional RRS      : %u\n",ntohs(dns->num_additional_rrs));
-    printf("Domain                  : ");
+    printf("  * Transaction ID          : 0x%04X\n", ntohs(dns->id));
+    printf("  * Flag                    : %u\n", ntohs(dns->flag));
+    printf("  * Questions               : %u\n", ntohs(dns->qdcount));
+    printf("  * Answers                 : %u\n", ntohs(dns->ancount));
+    printf("  * Num Authority RRS       : %u\n", ntohs(dns->num_authority_rrs));
+    printf("  * Num Additional RRS      : %u\n",ntohs(dns->num_additional_rrs));
+    printf("  * Question                : \n");
 
-    const unsigned char* question = dns_payload + sizeof(dnshdr);
-    unsigned char len = question[0];
+    const unsigned char *question = dns_payload + sizeof(dnshdr); 
+    question = parse_question(question);
 
-    while(*question != 0){
-        len = question[0];
-        for(int i = 1; i <= len; i++){
-            printf("%c", question[i]);
-        }
-        question += len + 1;
-        
-        if(*question != 0){
-            printf(".");
-        }   
+    printf("  * Anwser                  : \n");
+    /*
+    for (int i = 0; i < ntohs(dns->ancount); i++){
+        answer = parse_answer(answer);
     }
-    printf("\n");
+    */
+
     printf("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
 
 }
+
+const unsigned char *parse_question(const unsigned char *question){
+    printf("      - Domain : ");
+
+    while (*question != 0)
+    {
+        unsigned char len = *question;
+        question++;
+
+        for (int i = 0; i < len; i++)
+        {
+            printf("%c", question[i]);
+        }
+
+        question += len;
+
+        if (*question != 0)
+            printf(".");
+    }
+
+    printf("\n");
+
+    question++;      // skip terminating 0
+
+    uint16_t qtype = ntohs(*(uint16_t *)question);
+    question += 2;
+
+    uint16_t qclass = ntohs(*(uint16_t *)question);
+    question += 2;
+
+    printf("      - QTYPE  : %u\n", qtype);
+    printf("      - QCLASS : %u\n", qclass);
+
+    return question;
+}
+
+/*
+const unsigned char *parse_answer(const unsigned char *answer){
+    // Answer
+    question += 2;
+}
+*/
