dedcbbaf449a08f7e90815badb87922d2266b106
Saad Larhrib <saad.larhrib06@gmail.com>
2026-07-27 23:12:30 +0100

    Working to parse the arp header protocol

---
+++ ethernet/arp/arp.c
@@ -0,0 +1,16 @@
+/*struct arphdr{
+    uint16_t hardware_type;
+    uint16_t Protocol_type;
+    uint8_t  hardware_len;
+    uint8_t  protocol_len;
+    uint16_t operation;
+    uint32_t sender_hard_add;
+    uint32_t sender_prot_add;
+    uint32_t target_prot_add;
+    uint32_t target_prot_add;
+
+};
+
+void parse_arp(unsigned char ip_pyload, size_t remaining_size){
+
+}*/
